97fdd4bfe89e13d2f8c7abcdf67d5e69312bd4f1
Ao002 <114183307+Ao002@users.noreply.github.com>
2022-09-22 14:35:56 -0400

    Create OffenbergerLab2.java

---
+++ OffenbergerLab2.java
@@ -0,0 +1,7 @@
+class OffenbergerLab2 {
+    public static void main(String[] args) {
+        System.out.println("Name: Austin Offenberger"); 
+        System.out.println("Major: Computer Science");
+        System.out.println("Expected First Job: Software Engineer");
+    }
+}
