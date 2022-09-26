f8eea40c8f9625a964e25d6c3ec4e55ce0f54217
ryleywarner <71730190+ryleywarner@users.noreply.github.com>
2022-09-26 18:01:13 -0400

    Add files via upload

---
+++ WarnerLab2.java
@@ -0,0 +1,9 @@
+package warnerLab2;
+
+public class WarnerLab2 {
+	public static void main(String[] args) {
+        System.out.println("Name: Ryley Warner"); 
+        System.out.println("Major: Computer Science");
+        System.out.println("First Job: Software Developer");
+    }
+}
