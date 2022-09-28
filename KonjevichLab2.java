23a373144d99bf3dc4c8afc8f0c5c6bb28c7b401
AcKonjevich <114545694+AcKonjevich@users.noreply.github.com>
2022-09-27 22:52:13 -0400

    Add files via upload

---
+++ KonjevichLab2.java
@@ -0,0 +1,7 @@
+class KonjevichLab2 {
+    public static void main(String[] args) {
+        System.out.println("Name: Andrew Konjevich"); 
+        System.out.println("Major: Information Systems");
+        System.out.println("Expected First Job: Data Management");
+    }
+}
